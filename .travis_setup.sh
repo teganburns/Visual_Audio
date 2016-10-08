05a51422ca18c464d3daa11cd8b221b7880321f3
teganburns <teganburns@gmail.com>
2016-10-07 19:55:58 -0700

    working on travis

--- .travis_setup.sh
+++ .travis_setup.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-wget http://www.sfml-dev.org/files/SFML-2.4.0-linux-gcc-64-bit.tar.gz
-tar xvf SFML-2.4.0-linux-gcc-64-bit.tar.gz
-cd SFML-2.4.0
-sudo cp -r * /usr/
-sudo cp -r * /usr/local/
+git clone https://github.com/SFML/SFML.git
+cd SFML
+cmake .
+make
 cd ..
-ldconfig
 
